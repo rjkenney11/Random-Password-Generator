6cc099ffe3c3a6c726087578e2d675400ec85814
Rachel Kenney <rkenney@howardcountymd.gov>
2020-12-21 19:44:22 -0500

    generate

--- script.js
+++ script.js
@@ -1,41 +1,83 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var userPswd; 
+var userPswd = "";
 
-var upper= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lower= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var spchar= ["!", "@", "#", "$", "%", "^", "&", "*", "_", "-", "+", "="];
-var number= ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-var inclup= incllow= inclspc= inclnum= false;
-var pswdlngth= 0;
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var spchar = ["!", "@", "#", "$", "%", "^", "&", "*", "_", "-", "+", "="];
+var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var inclup = incllow = inclspc = inclnum = false;
+var pswdlngth = 0;
 
+function consoleInside(arr) {
+    console.log("arr", arr);
+    console.log("arr.length", arr.length);
+    for (i = 0; i < arr.length; i++) {
+        console.log("i: ", i);
+        console.log(arr[i]);
+    }
+}
 
+consoleInside(upper);
+consoleInside(lower);
+consoleInside(spchar);
+consoleInside(number);
+var pswdLength;
+var selectup;
+var selectlow;
+var selectspec;
+var selectnum;
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
-}
-
-function generatePassword (){
-    var user = prompt("How many characters would you like your password to be? (Choose a number between 8-128)");
-        if (user < 8 || user > 128){
+    function promptanswers() {
+        pswdLength = prompt("How many characters would you like your password to be? (Choose a number between 8-128)");
+        if (pswdLength < 8 || pswdLength > 128) {
             alert("Invalid password length");
-        while (inclup === false, incllow === false, inclspec === false, inclnum ===false);
+            return;
         }
-    var user = prompt("Would you like to include uppercase letters in your password?");
-    var user = prompt("Would you like to include lowercase letters in your password?");
-    var user = prompt("Would you like to include special characters in your password?");
-    var user = prompt("Would you like to include numbers in your password?");
+        selectup = confirm("Would you like to include uppercase letters in your password?");
+        selectlow = confirm("Would you like to include lowercase letters in your password?");
+        selectspec = confirm("Would you like to include special characters in your password?");
+        selectnum = confirm("Would you like to include numbers in your password?");
+    }
+
+    passwordText.value = password;
+
+    function generatePassword() {
+        promptanswers();
+        var possibleChar = [];
+        console.log(possibleChar);
+
+        if (selectup) {
+            possibleChar = possibleChar.concat(upper);
+        }
+        if (selectlow) {
+            possibleChar = possibleChar.concat(lower);
+        }
+        if (selected) {
+            possibleChar = possibleChar.concat(spchar);
+        }
+        if (selected) {
+            possibleChar = possibleChar.concat(number);
+        }
+
+        
+        for(var i = 0; i<l; i++){
+                userPswd += possibleChar.charAt(Math.floor(Math.random() * pswdLength.length));
+        }
 
 
 
-    return userPswd;
-}
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+        return userPswd;
+    }
+
+
+
+    // Add event listener to generate button
+    generateBtn.addEventListener("click", writePassword);
